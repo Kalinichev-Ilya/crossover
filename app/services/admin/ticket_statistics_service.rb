44e7ba7717ddd5f5aee913826014198a15a0cbea
Ilya Kalinichev <iluya.kalinichev@gmail.com>
2017-10-15 15:52:42 +0300

    ignore idea

--- app/services/admin/ticket_statistics_service.rb
+++ app/services/admin/ticket_statistics_service.rb
@@ -5,15 +5,15 @@ module Admin
       @to = to.present? ? Date.parse(to) : Date.tomorrow
       @user_id = Admin.exists?(id: user_id) && user_id
     end
-    
+
     def call
       tickets = Tickets.completed.where('date(updated_at) >= :from AND date(updated_at) <= :to', from: from, to: to)
       tickets = tickets.where(admin_id: user_id) if user_id
       tickets.reorder(admin_id: :desc, updated_at: :desc)
     end
-    
+
     private
-    
+
     attr_reader :from, :to, :user_id
   end
 end
